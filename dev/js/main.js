41fb15a2041cd3dd5aa22b9d0ad6ab4ca03b3420
grammlaman <K1j1u1j1n1b1g1>
2020-01-27 18:26:08 +0600

    Первая версия разметки и стили под мобилу закончены

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,10 +1,42 @@
-class user {
-    constructor() {
+class EventBus{
+    constructor(){
         const _ = this;
-        _.type = [guest, user, superUser];
+        _.events = {};
+    }
+    add(event,fn){
+        const _ = this;
+        if(!_.events[event]) _.events[event] = [];
+        _.events[event].push(fn);
+    }
+    trigger(event,data=null){
+        const _ = this;
+        console.warn("Сработало событие: " + event);
+        if(!_.events[event]) return;
+        for(let i = 0; i < _.events[event].length;i++){
+            console.log(_.events[event][i]);
+            _.events[event][i](data);
+        }
+    }
+    remove(event,fn){
+        const _ = this;
+        if(_.events[event]){
+            for(let i = 0; i < _.events[event].length; i++){
+                if(_.events[event][i] === fn){
+                    _.events[event].splice(index(_.events[event][i]));
+                }
+            }
+        }
+    }
+}
+const Bus = new EventBus();
+class User{
+    constructor(){
+        const _ = this;
+        _.type = ['guest','user'];
     }
     loginAsGuest(type){
         const _ = this;
+        console.log(_.type);
         if(type === _.type[0]){
 
         } else {
@@ -15,18 +47,10 @@ class user {
         const _ = this;
         if(type === _.type[1]){
 
-        } else {
-            _.loginAsSuperUser(type);
-        }
-    }
-    loginAsSuperUser(type){
-        const _ = this;
-        if(type === _.type[2]){
-
-        }
+        } else return;
     }
 }
-
-let list = {
-
-};
+let a = new User;
+let eventBus = new EventBus();
+eventBus.add('click',a.loginAsGuest);
+eventBus.trigger('click');
