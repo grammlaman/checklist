d6073a0e8beb5f87d83db050e38477d344b9c651
grammlaman <K1j1u1j1n1b1g1>
2020-01-28 00:48:00 +0600

    Первая версия разметки и стили под мобилу закончены

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,54 +1,76 @@
-class EventBus{
-    constructor(){
+let months = {
+    ru : [
+        'Январь',
+        'Февраль',
+        'Март',
+        'Апрель',
+        'Май',
+        'Июнь',
+        'Июль',
+        'Август',
+        'Сентябрь',
+        'Октябрь',
+        'Ноябрь',
+        'Декабрь'
+    ]
+};
+class List {
+    constructor() {
         const _ = this;
-        _.events = {};
+        _.date = new Date();
+        _.year = _.date.getFullYear();
+        _.month = _.date.getMonth();
+        _.dayInMonth = _.date.getDate();
+        _.dayInWeek = _.date.getDay();
+        _.calendar = {
+            body : document.querySelector('#calendar'),
+            monthSelect : document.querySelector('#monthSelect'),
+            yearSelect : document.querySelector('#yearSelect'),
+            daysInMonth : document.querySelector('.calendar-month')
+        };
     }
-    add(event,fn){
+    //Метод отприсовки дней в месяце
+    drawDays(month,year){
         const _ = this;
-        if(!_.events[event]) _.events[event] = [];
-        _.events[event].push(fn);
-    }
-    trigger(event,data=null){
-        const _ = this;
-        console.warn("Сработало событие: " + event);
-        if(!_.events[event]) return;
-        for(let i = 0; i < _.events[event].length;i++){
-            _.events[event][i](data);
+        let days = 31;
+        if((month == 3) || (month == 5) || (month == 8) || (month == 10)){
+            days = 30;
+        } else if((month == 1)){
+            if((year % 4) == 0){
+                days = 29
+            } else {days = 28}
+        }
+        _.calendar.daysInMonth.innerHTML = '';
+        for(let i = 0; i < days; i++){
+            let btn = document.createElement('BUTTON'),
+                day = document.createElement('SPAN'),
+                tasks = document.createElement('SPAN'),
+                cnt = i + 1;
+                if(cnt < 10) cnt = '0' + cnt;
+                btn.className = 'calendar-month-day';
+                day.className = 'calendar-month-day-number';
+                day.textContent = cnt;
+                tasks.className = 'calendar-month-day-do';
+                tasks.textContent = 'Дел: 0';
+                btn.append(day,tasks);
+                _.calendar.daysInMonth.append(btn)
         }
     }
-    remove(event,fn){
+    init(){
         const _ = this;
-        if(_.events[event]){
-            for(let i = 0; i < _.events[event].length; i++){
-                if(_.events[event][i] === fn){
-                    _.events[event].splice(index(_.events[event][i]));
-                }
-            }
-        }
+        _.drawDays(_.month,_.year)
     }
 }
-const Bus = new EventBus();
-class User{
-    constructor(){
-        const _ = this;
-        _.type = ['guest','user'];
-    }
-    async checkLogin(){
-        const _ = this;
-        let req = await _.xhr.fetch('JSON',{
-            path : '/front/components/user/assets/checkLogin.php',
-            data : null
-        });
-        _.role = req['role'];
-        if(req['role'] === 'guest'){
-            MainEventBus.trigger('User','enteredAsGuest',req);
-        } else {
-            MainEventBus.trigger('User','enteredAsUser',req)
-        }
-    }
+let list = new List();
+list.init();
 
-}
-let a = new User;
-let eventBus = new EventBus();
-eventBus.add('click',a.loginAsGuest);
-eventBus.trigger('click');
+document.querySelector('.calendar-month').addEventListener('click',function (el) {
+    let clickTarget = el.target;
+    console.log(clickTarget);
+    if(clickTarget == document.querySelector('.calendar-month')) return;
+    else if(clickTarget == document.querySelector('.calendar-month-day')) return;
+    else {
+        clickTarget = clickTarget.parentElement;
+    }
+    console.log(clickTarget)
+});
