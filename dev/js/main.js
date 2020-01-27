c3a97c69e9ec754995761129ec219e7d17bb8d84
grammlaman <K1j1u1j1n1b1g1>
2020-01-27 16:25:41 +0600

    Первая версия разметки и стили под мобилу закончены

--- dev/js/main.js
+++ dev/js/main.js
@@ -3,4 +3,30 @@ class user {
         const _ = this;
         _.type = [guest, user, superUser];
     }
+    loginAsGuest(type){
+        const _ = this;
+        if(type === _.type[0]){
+
+        } else {
+            _.loginAsUser(type);
+        }
+    }
+    loginAsUser(type){
+        const _ = this;
+        if(type === _.type[1]){
+
+        } else {
+            _.loginAsSuperUser(type);
+        }
+    }
+    loginAsSuperUser(type){
+        const _ = this;
+        if(type === _.type[2]){
+
+        }
+    }
 }
+
+let list = {
+
+};
